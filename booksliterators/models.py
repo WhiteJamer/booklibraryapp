cb5a378f1d22f9b8c626dd9295738910e16c8235
whitejamer <muxah3258@gmail.com>
2019-12-24 21:35:24 +0300

    Change models & make migrations

--- booksliterators/models.py
+++ booksliterators/models.py
@@ -1,25 +1,27 @@
 from django.db import models
-
-
-class Vote(models.Model):
-    RATING_CHOICES = [1, 2, 3, 4, 5]
-    value = models.PositiveIntegerField(choices=RATING_CHOICES)
-    book = models.ForeignKey(Book, related_name='votes', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return '{0} - {1}'.format(self.value, self.book.name)
+from django.db.models import Avg
 
 
 class Book(models.Model):
     name = models.CharField(max_length=255)
+    rating = models.IntegerField(null=True)
 
     def __str__(self):
-        return self.name
+        return '{0} - {1}'.format(self.pk, self.name)
 
 
 class Literator(models.Model):
     name = models.CharField(max_length=255)
-    books = models.ManyToManyField(Book, related_name='literators')
+    books = models.ManyToManyField(Book, related_name='literators', blank=True)
 
     def __str__(self):
-        return self.name
+        return '{0} - {1}'.format(self.pk, self.name)
+
+
+class Vote(models.Model):
+    RATING_CHOICES = CHOICES = [(i, i) for i in range(1, 6)]  # choices 1-5
+    value = models.PositiveIntegerField(choices=RATING_CHOICES)
+    book = models.ForeignKey(Book, related_name='votes', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return '{0} - {1}'.format(self.value, self.book.name)
