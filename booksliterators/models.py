c92ebdfd1e6c44dd90063806e98e4c06d5265245
whitejamer <muxah3258@gmail.com>
2019-12-24 11:52:49 +0300

    Design models Vote, Book, Literator

--- booksliterators/models.py
+++ booksliterators/models.py
@@ -1,4 +1,25 @@
 from django.db import models
 
 
+class Vote(models.Model):
+    RATING_CHOICES = [1, 2, 3, 4, 5]
+    value = models.PositiveIntegerField(choices=RATING_CHOICES)
+    book = models.ForeignKey(Book, related_name='votes', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return '{0} - {1}'.format(self.value, self.book.name)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Literator(models.Model):
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book, related_name='literators')
+
+    def __str__(self):
+        return self.name
